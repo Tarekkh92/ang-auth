536c0b78db3f614a441213601ea4c9a3459c5795
tarek khalaily <khalailyt@gmail.com>
2021-02-10 11:53:22 +0200

    make it better

---
+++ src/app/auth/user-guard.ts
@@ -0,0 +1,19 @@
+// import {Injectable} from '@angular/core'
+// import {CanActivate, Router,ActivatedRouteSnapshot,}
+// Injectable({
+//     providedIn: 'root'
+// })
+
+// export class UserGuard implements CanActivate{
+//     user = 'local'
+//     constructor(private router:Router){}
+// canActivate(next : ActivatedRouteSnapshot){
+// if(this.user == 'local'){
+//     return true
+// }else{
+//     this.router.navigateByUrl('login')
+
+//     return true
+// }
+    
+// }
